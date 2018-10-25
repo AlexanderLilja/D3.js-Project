fcfad44f1da76da443dbca6b0a7b4e105c65b563
Alexander <liljaale@arcada.fi>
2018-10-25 19:25:54 +0300

    Final version
    Everything works 100 %

--- graph.js
+++ graph.js
@@ -1,95 +1,101 @@
-//Track window size
+//TRACK WINDOW SIZE
 window.watchResize(() => {
 
 
-    //EMPTY OLD CONTENT BEFORE RENDERING NEW 
-    $('body').html("");
+    //EMPTY CONTENT BEFORE RENDERING NEW 
+    $('body').html('<div id="tooltip"></div>');
 
 
-    //Read in json file with promise
+    //READ IN JSON FILE WITH PROMISE
     d3.json("facebookFriends.json").then((data) => {
 
-        //Settings for canvas    
+
+        //SETTINGS CONTAINER    
         var settings = {
             color: "lightgrey",
             height: window.innerHeight,
             width: window.innerWidth,
-            padding: 20,
-            size: { 
-                small: 16,
-                medium: 60,
-                large: 88 
-            }
+            padding: 10,
+            node: {
+                background: 'yellow',
+                hover: '#B1E9C8',
+                multiplier: 15
+            },
+            border: {
+                color: '#2c3539',
+                size: 2
+            },
+            font: {
+                family: 'Open Sans',
+                size: 15,
+                color: 'red',
+                weight: 600
+            },
+            distance: 10,
+            collision: 6
         }
 
-        //Create variables to use with json data
+        //CONTAINERS
         var nodes = [];
         var links = [];
-        
 
-        //loop through all entries
+
+        //LOOP THROUGH ALL ENTRIES
         for (let key in data.facebookFriends) {
             let name = data.facebookFriends[key].name
             let id = data.facebookFriends[key].FacebookID
             let friends = data.facebookFriends[key].friends
-            let weight = friends.length * 4
-            
-            //Push data in nodes array
+
+
+            //PUSH DATA TO NODES ARRAY
             nodes.push({
                 'id': id,
                 'name': name,
-                'weight': weight
+                'friends': friends.length
             })
 
-            //get the friends for each entry and create the links
+            //LOOP THROUGH FRIENDS
             for (let i in friends) {
                 let friend = friends[i].FriendId
 
-                //Push data in links array
+                //PUSH DATA TO LINKS ARRAY
                 links.push({
                     'source': id,
-                    'target': friend,
-                    'weight': weight
+                    'target': friend
                 })
 
             }
         }
-    
-        //testing
-        console.log(nodes);
-        console.log(links);
-        
-        var size = nodeSize(nodes, settings);
 
-        //Generate canvas
+
+        //GENERATE CANVAS
         var canvas = d3.select("body").append("svg")
-            .attr("height", settings.height - settings.padding)
-            .attr("width", settings.width - settings.padding)
+            .attr("height", settings.height - (settings.padding * 2))
+            .attr("width", settings.width - (settings.padding * 2))
             .style("background", settings.color)
 
 
-        //Simulate nodes and links
+        //SIMULATE LINKS AND NODES
         var simulation = d3.forceSimulation(nodes)
             .force("charge", d3.forceManyBody().strength(-40))
             .force("center", d3.forceCenter(settings.width / 2, settings.height / 2))
-            .force("collision", d3.forceCollide().radius(() => {
-                return size * 6
-            }))
+            .force("collision", d3.forceCollide().radius(settings.collision * settings.node.multiplier))
             .force("link", d3.forceLink().id((d) => {
-                return d.id;
-            }).distance(() => {
-                return size / 5
-            }))
+                    return d.id;
+                })
+                .distance(settings.distance * settings.node.multiplier))
+
+            //RENDER SIMULATION
             .on("tick", ticked)
 
 
-        //Just to make things easier down the line       
+        // VARIABLES TO STORE SPECIFIC ELEMENTS       
         var link = canvas.append("g").attr("class", "links").selectAll("link"),
             node = canvas.append("g").attr("class", "node").selectAll("node"),
             label = canvas.append("g").attr("class", "labels").selectAll("rect"),
             text = canvas.append("g").attr("class", "text").selectAll("text");
 
-        //Using force properties on links and nodes
+        //USING FORCE PROPERTIES ON LINKS AND NODES
         simulation.nodes(nodes)
         simulation.force("link").links(links);
 
@@ -103,14 +109,15 @@ window.watchResize(() => {
 
         //Append labels to each entry
         label = label.data(nodes)
-            .enter().append("rect").style("visibility", "hidden");
+            .enter().append("rect");
 
         //Append text to each entry
         text = text.data(nodes)
-            .enter().append("text").style("visibility", "visible");
+            .enter().append("text");
 
         //Draw the links and nodes etc
         function ticked() {
+
 
             //Properties for links
             link.attr("x1", (d) => {
@@ -125,63 +132,71 @@ window.watchResize(() => {
                 .attr("y2", (d) => {
                     return d.target.y;
                 })
-                .attr("stroke", "#2c3539")
-                .attr("stroke-width", size / 2 );
+                .attr("stroke", settings.border.color)
+                .attr("stroke-width", settings.border.size)
+                .style("pointer-events", "none");
 
 
             //Properties for nodes    
-            node.attr('r', size)
+            node.attr('r', (d) => {
+                    return d.friends * settings.node.multiplier
+                })
                 .attr('cx', (d) => {
                     return d.x;
                 })
                 .attr('cy', (d) => {
                     return d.y;
                 })
-                .attr("stroke", "black")
-                .attr("stroke-width", (d) => {
-                    return size / 1.5
+                .attr("stroke", settings.border.color)
+                .attr("stroke-width", settings.border.size)
+                .attr("fill", settings.node.background)
+
+                // MOUSEOVER TOOLTIP PROPERTIES
+                .on('mouseover', function(d) {
+                    d3.select(this).attr('fill', settings.node.hover)
+                    $('#tooltip').html(d.name + ' (' + d.friends + ')')
+                    $('#tooltip').css('left', d3.event.pageX - ($('#tooltip').width() / 1.5) + 'px')
+                    $('#tooltip').css('top', d3.event.pageY + 20 + 'px')
+                    $('#tooltip').css('opacity', 1)
                 })
-                .attr("fill", "gray")
-                .on("mouseover", () => {
-                    return text.style("visibility", "visible"),
-                        label.style("visibility", "hidden");
+
+                 // MOUSEOUT
+                .on('mouseout', function () {
+                    d3.select(this).attr('fill', settings.node.background)
+                    $('#tooltip').css('opacity', 0)
                 })
-                .on("mouseout", () => {
-                    return text.style("visibility", "visible"),
-                        label.style("visibility", "hidden");
-                });
+
 
             //Properties for label
             label.attr('x', (d) => {
-                    return d.x - d.name.length;
+                    return d.x;
                 })
                 .attr('y', (d) => {
-                    return d.y - d.weight * 3;
+                    return d.y;
                 })
-                .attr("width", (d) => {
-                    return d.name.length * d.weight * 3;
-                })
-                .attr("height", (d) => {
-                    return size
-                })
-                .style("fill", "white");
+                .attr("width", 50)
+                .attr("height", 50)
+                .style("fill", "none")
+                .style("pointer-events", "none");
 
             //Properties for text
             text.attr('dx', (d) => {
                     return d.x;
                 })
                 .attr('dy', (d) => {
-                    return d.y;
+                    return d.y + 5;
                 })
                 .text((d) => {
-                    return d.name;
+                    return d.name[0];
                 })
-                .style("font-size", size * 3)
-                .style("font-weight", 600)
-                .style("text-anchor", "middle");
+                .style("font-size", settings.font.size)
+                .style("font-weight",settings.font.weight)
+                .style("text-anchor", "middle")
+                .style("pointer-events", "none");
 
 
         }
+
 
 
     });
